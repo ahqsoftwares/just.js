9f9755f92a15e4c286fe8726899f9766ecc031c7
Akshanabha <84524025+ahqsoftwares@users.noreply.github.com>
2022-09-30 14:15:00 +0530

    publish to actions

--- publish.js
+++ publish.js
@@ -1,10 +1,11 @@
-const { exec } = require("child_process");
+const { exec, execSync } = require("child_process");
 
 const version = require("./package.json").version;
 const chalk = require("chalk");
 
 function publish(tag) {
          console.log(`Publishing @${chalk.green(tag ? tag : "latest")}`);
+         execSync(`npm adduser`)
          exec(`npm publish${tag ? ` --tag=${tag}` : ""}`, {env: process.env}, (err, stderr, out) => {
                   if (err) {
                            console.log(chalk.red(String(err)));
