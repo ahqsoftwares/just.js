413b6466e6b57728c4d8c2864665277259768ee7
Akshanabha <84524025+ahqsoftwares@users.noreply.github.com>
2022-09-30 14:34:57 +0530

    add token

--- publish.js
+++ publish.js
@@ -3,8 +3,6 @@ const { exec } = require("child_process");
 const version = require("./package.json").version;
 const chalk = require("chalk");
 
-process.env.NPM_TOKEN = "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";
-
 function publish(tag) {
          console.log(`Publishing @${chalk.green(tag ? tag : "latest")}`);
          exec(`npm publish${tag ? ` --tag=${tag}` : ""}`, {env: process.env}, (err, stderr, out) => {
